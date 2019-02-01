268733940fb336b36f4039cfdd9268fced0062f8
Martin Nordsletten <mnordsletten@gmail.com>
2019-02-01 14:31:40 +0100

    Build binutils: Start of conan create

--- functions.groovy
+++ functions.groovy
@@ -1,32 +1,6 @@
 #!/usr/bin/env groovy
 
-def build(versions, architectures, build_types) {
-  versions = "${versions}".replaceAll("\\s", "").split(',')
-  architectures = "${architectures}".replaceAll("\\s", "").split(',')
-  build_types = "${build_types}".replaceAll("\\s", "").split(',')
-
-  def builds = [:]
-
-  for (ver in versions) {
-    for (arch in architectures) {
-      for (build in build_types) {
-        String buildName = "${build}-${arch}-${ver}"
-
-        builds[buildName] = {
-          node('conan_pipe_worker') {
-            stage(buildName) {
-              echo buildName
-            }
-          }
-        }
-
-      }
-    }
-  }
-  parallel builds
-}
-
-def create_binutils_build_commands(version, profiles, target_oss, target_architectures, build_types, conanfile_path) {
+def create_build_commands(version, profiles, target_oss, target_architectures, build_types, conanfile_path) {
   // clean the input parameters
   profiles = "${profiles}".replaceAll("\\s", "").split(',')
   target_oss = "${target_oss}".replaceAll("\\s", "").split(',')
@@ -46,6 +20,41 @@ def create_binutils_build_commands(version, profiles, target_oss, target_archite
             echo "buildName: ${buildName}"
             echo "conanfile path: ${conanfile_path}"
             cat "${conanfile_path}"
+          """
+        }
+      }
+    }
+  }
+
+  return builds
+}
+
+
+def create_binutils_build_commands(version, profiles, target_oss, target_architectures, build_types, conanfile_path) {
+  // clean the input parameters
+  profiles = "${profiles}".replaceAll("\\s", "").split(',')
+  target_oss = "${target_oss}".replaceAll("\\s", "").split(',')
+  target_architectures = "${target_architectures}".replaceAll("\\s", "").split(',')
+  build_types = "${build_types}".replaceAll("\\s", "").split(',')
+
+  // Loop to create all build tasks
+  def builds = [:]
+
+  for (prof in profiles) {
+    for (t_os in target_oss) {
+      for (t_arch in target_architectures) {
+        for (b_type in build_types) {
+          String buildName = "${prof}-${b_type}-${t_os}-${t_arch}"
+          String buildCmd = "conan create ${conanfile_path} -pr ${prof}"
+
+          if (b_type.length() > 0) {
+            buildCmd += " -s build_type=${b_type}"
+          }
+          if (t_os.length() > 0) {
+            buildCmd += " -s os=${t_os}"
+          }
+          builds[buildName] = """
+            echo "${buildCmd}"
           """
         }
       }
