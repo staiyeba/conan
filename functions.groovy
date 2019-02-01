86b521c444f467d447de776471c79cd699b1d69f
Martin Nordsletten <mnordsletten@gmail.com>
2019-02-01 15:01:39 +0100

    Functions: Remove unused build function

--- functions.groovy
+++ functions.groovy
@@ -1,35 +1,5 @@
 #!/usr/bin/env groovy
 
-def create_build_commands(version, profiles, target_oss, target_architectures, build_types, conanfile_path) {
-  // clean the input parameters
-  profiles = "${profiles}".replaceAll("\\s", "").split(',')
-  target_oss = "${target_oss}".replaceAll("\\s", "").split(',')
-  target_architectures = "${target_architectures}".replaceAll("\\s", "").split(',')
-  build_types = "${build_types}".replaceAll("\\s", "").split(',')
-
-  // Loop to create all build tasks
-  def builds = [:]
-
-  for (prof in profiles) {
-    for (t_os in target_oss) {
-      for (t_arch in target_architectures) {
-        for (b_type in build_types) {
-          String buildName = "${prof}-${b_type}-${t_os}-${t_arch}"
-
-          builds[buildName] = """
-            echo "buildName: ${buildName}"
-            echo "conanfile path: ${conanfile_path}"
-            cat "${conanfile_path}"
-          """
-        }
-      }
-    }
-  }
-
-  return builds
-}
-
-
 def create_binutils_build_commands(version, profiles, target_oss, target_architectures, build_types, conanfile_path, conan_user, conan_channel) {
   // clean the input parameters
   profiles = "${profiles}".replaceAll("\\s", "").split(',')
